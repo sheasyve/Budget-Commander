5ce8ec488e2a25a59bb141c94b7440efa2646d54
sheasyve <113799482+sheasyve@users.noreply.github.com>
2023-02-26 00:23:35 -0800

    Update QuickBudget.py

--- QuickBudget.py
+++ QuickBudget.py
@@ -10,9 +10,14 @@ class expense:
         self.cost = cost
         self.description = description
 
-def expense_prompt(name,cost,costin,saved_expenses):
+def remove_expense(expenses,name):
+    for i,e in enumerate(expenses):
+        if e.name == name:
+            del expenses[i]
+            return expenses
+
+def expense_prompt(name,cost,costin,saved_expenses,managing):
     name = input("Enter the name of the expense\n")
-    print(name)
     saved_expense = esearch_e(saved_expenses,name)
     if saved_expense != None:
         return saved_expense
@@ -21,12 +26,17 @@ def expense_prompt(name,cost,costin,saved_expenses):
         costin = input("Cost must be a number\n")
     cost = float(costin)
     description = input("Enter a description or leave it blank\n")
-    command = input("Enter s to save the expense for reuse or enter to continue\n")
-    if command == "s" or command == "S":
+    command = ""
+    if managing == False:
+        command = input("Enter s to save the expense for reuse or enter to continue\n")
+    if command == "s" or command == "S" or managing:
+        saved_expences = remove_expense(saved_expenses,name)
         if description == "":
-            save_expense(expense(name,cost,name))
+            saved_expenses.append(expense(name,cost,name))
+            save_expenses(saved_expenses)
         else:
-            save_expense(expense(name,cost,description))
+            saved_expenses.append(expense(name,cost,description))
+            save_expenses(saved_expenses)
     if description == "":
         return expense(name,cost,name)
     else:
@@ -48,32 +58,30 @@ def list_expenses(expenses):
 
 def esearch_e(expenses,name):
     for e in expenses:
-        print(e.name + "en")
-        print(name)
-        if e.name == name:
-            print(1)
+        if re.fullmatch(e.name,name, re.IGNORECASE):
             return e
-        print(2)
     return None
 
 def esearch(expenses):
-    ename = input("Enter the name of the expense to modify or -1 to go back\n")
-    if ename == "-1":
-        return -1
+    ename = input("Enter the name of the expense to modify or 0 to go back\n")
+    if ename == "0":
+        return "-1"
     expense = esearch_e(expenses, ename)
     while expense == None:
-        ename = input("Expense not found, enter an expense or -1 to go back\n")
-        if ename == "-1":
-            return -1
+        ename = input("Expense not found, enter an expense or 0 to go back\n")
+        if ename == "0":
+            return "-1"
         expense = esearch_e(expenses, ename)
     return expense
 
 def modify_expense(expenses):
     expense = esearch(expenses)
-    command = input("Enter 1 to modify the name, enter 2 to modify the cost, enter 3 to modify description, or -1 to go back\n")
-    while re.match(r"1||2||3||-1",command) == None:
+    command = "0"
+    if expense != "-1":
+        command = input("Commands:\n1 - Modify the name, 2 - to modify the cost, 3 - Modify description, 0 - Go back\n")
+    while re.match(r"1||2||3||0",command) == None:
         print("Incorrect command")
-        command = input("Enter 1 to modify the name, enter 2 to modify the cost, enter 3 to modify description, or -1 to go back\n")
+        command = input("Commands:\n1 - Modify the name, 2 - to modify the cost, 3 - Modify description, 0 - Go back\n")
     if command == "1":
         new_name = input("Enter the new name\n")
         expense.name = new_name
@@ -98,23 +106,30 @@ def make_budget(saved_expenses):
     manage_budget(funds,expenses,saved_expenses)
 
 def manage_budget(funds,expenses,saved_expenses):
-    command = input("Enter 1 to add an expense, 2 to view the current budget, 3 to modify an expense, or -1 to return to the main menu\n")
-    while command != "-1":
+    command = input("Commands:\n1 - Add an expense, 2 - View the current budget, 3 - List saved expenses,\n4 - Modify budget expense, 5 - Modify saved expense, 0 - Go back\n")
+    while command != "0":
         if command == "1":
             name,description,cost = "","",0
-            expenses.append(expense_prompt(name,cost,description,saved_expenses))
+            expenses.append(expense_prompt(name,cost,description,saved_expenses,False))
         elif command == "2":
             display_budget(expenses,funds)
         elif command == "3":
+            list_saved_expenses(saved_expenses)
+        elif command == "4":
             list_expenses(expenses)
             modify_expense(expenses)
+        elif command == "5":
+            list_expenses(saved_expenses)
+            modify_expense(saved_expenses)
+            save_expenses(saved_expenses)
         else:
             print("You have entered an incorrect command")
-        command = input("Enter 1 to add an expense, 2 to view the current budget, 3 to modify an expense, or -1 to return to the main menu\n")
+        command = input("Commands:\n1 - Add an expense, 2 - View the current budget, 3 - List saved expenses,\n4 - Modify budget expense, 5 - Modify saved expense, 0 - Go back\n")
 
-def save_expense(expense):
-    f = open('expenses.txt', 'a', encoding="utf-8")
-    f.write("n\n"+ expense.name +"\nc\n" + str(expense.cost) + "\nd\n" + expense.description + "\n")
+def save_expenses(expenses):
+    f = open('expenses.txt', 'w', encoding="utf-8")
+    for e in expenses:
+        f.write("n\n"+ e.name +"\nc\n" + str(e.cost) + "\nd\n" + e.description + "\n")
     f.close()
 
 def load_expenses():
@@ -142,14 +157,15 @@ def clear_expenses(saved_expenses):
     f.close()
 
 def manage_saved_expenses(saved_expenses):
-    command = input("Enter 1 to add an expense, 2 to modify expense, 3 to list expenses, 4 to clear saved expenses, or -1 to go back\n")
-    while command != "-1":
+    command = input("Commands: 1 - Add an expense, 2 - Modify expense, 3 - List expenses, 4 - Clear saved expenses, 0 - Go back\n")
+    while command != "0":
         if command == "1":
             name,description,cost = "","",0
-            e = expense_prompt(name,cost,description,saved_expenses)
+            e = expense_prompt(name,cost,description,saved_expenses,True)
             saved_expenses.append(e)
         elif command == "2":
             modify_expense(saved_expenses)
+            save_expenses(saved_expenses)
         elif command == "3":
             list_saved_expenses(saved_expenses)
         elif command == "4":
@@ -157,7 +173,7 @@ def manage_saved_expenses(saved_expenses):
             print("Expenses cleared")
         else:
             print("Incorrect command entered")
-        command = input("Enter 1 to add an expense, 2 to modify expense, 3 to list expenses, 4 to clear saved expenses, or -1 to go back\n")
+        command = input("Commands: 1 - Add an expense, 2 - Modify expense, 3 - List expenses, 4 - Clear saved expenses, 0 - Go back\n")
 
 def list_saved_expenses(saved_expenses):
     for e in saved_expenses:
@@ -166,9 +182,10 @@ def list_saved_expenses(saved_expenses):
         print("Description: " + e.description + "\n")
 
 def main():
-    userin = input("Enter 1 to make a new budget, 2 to manage saved expenses, or -1 to quit.\n")
+    print("Welcome to QuickBudget")
+    userin = input("Enter 1 to make a new budget, 2 to manage saved expenses, or 0 to quit.\n")
     saved_expenses = load_expenses()
-    while(userin != "-1"):
+    while(userin != "0"):
         if(userin == "1"):
             make_budget(saved_expenses)
         elif userin == "2":
@@ -176,7 +193,7 @@ def main():
             saved_expenses = load_expenses()
         else:
             print("You have entered an incorrect command")
-        userin = input("Enter 1 to make a new budget, 2 to manage saved expenses, or -1 to quit.\n")
+        userin = input("Commands:\n1 - Make a new budget, 2 - Manage saved expenses, 0 - Quit.\n")
     print("Goodbye")
 
 if __name__ == "__main__":
