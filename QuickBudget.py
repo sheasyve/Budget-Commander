f03cb39e9924859eb0713aaa00382431c097ab0b
sheasyve <113799482+sheasyve@users.noreply.github.com>
2023-02-26 22:13:36 -0800

    Update QuickBudget.py

--- QuickBudget.py
+++ QuickBudget.py
@@ -1,6 +1,8 @@
 #Quickbudget Budget Manager
 #Written by Shea Syverson 
 
+#Coming soon: Saving and exporting a budget to text, for reference or to reload and modify.
+
 import sys
 import re
 
@@ -11,12 +13,39 @@ class expense:
         self.description = description
 
 def remove_expense(expenses,name):
+    #Remove an expense from a list
     for i,e in enumerate(expenses):
         if e.name == name:
             del expenses[i]
             return expenses
 
+def list_expenses(expenses):
+    print("Expenses:")
+    for e in expenses:
+        print(e.name)
+
+def esearch_e(expenses,name):
+    #Iterate through list and find the expense
+    for e in expenses:
+        if re.fullmatch(e.name,name, re.IGNORECASE):
+            return e
+    return None
+
+def esearch(expenses):
+    #Prompts the user for an expense to find and calls the esearch_e function to find it
+    ename = input("Enter the name of the expense to modify or 0 to go back\n")
+    if ename == "0":
+        return "-1"
+    expense = esearch_e(expenses, ename)
+    while expense == None:
+        ename = input("Expense not found, enter an expense or 0 to go back\n")
+        if ename == "0":
+            return "-1"
+        expense = esearch_e(expenses, ename)
+    return expense
+
 def expense_prompt(name,cost,costin,saved_expenses,managing):
+    #This function provides the code to add and save a new expense, to the budget or to the file
     name = input("Enter the name of the expense\n")
     saved_expense = esearch_e(saved_expenses,name)
     if saved_expense != None:
@@ -30,7 +59,7 @@ def expense_prompt(name,cost,costin,saved_expenses,managing):
     if managing == False:
         command = input("Enter s to save the expense for reuse or enter to continue\n")
     if command == "s" or command == "S" or managing:
-        saved_expences = remove_expense(saved_expenses,name)
+        saved_expenses = remove_expense(saved_expenses,name)
         if description == "":
             saved_expenses.append(expense(name,cost,name))
             save_expenses(saved_expenses)
@@ -42,39 +71,8 @@ def expense_prompt(name,cost,costin,saved_expenses,managing):
     else:
         return expense(name,cost,description)
 
-def display_budget(expenses, funds):
-    remaining_funds = funds
-    print("\nStarting budget: " + str(funds))
-    for e in expenses:
-        new_funds = remaining_funds - e.cost
-        print(e.name + ": " + str(remaining_funds) + " - " + str(e.cost) + " = " + str(new_funds))
-        remaining_funds = new_funds
-    print("Total remaining funds: " + str(remaining_funds) + "\n")
-
-def list_expenses(expenses):
-    print("Expenses:")
-    for e in expenses:
-        print(e.name)
-
-def esearch_e(expenses,name):
-    for e in expenses:
-        if re.fullmatch(e.name,name, re.IGNORECASE):
-            return e
-    return None
-
-def esearch(expenses):
-    ename = input("Enter the name of the expense to modify or 0 to go back\n")
-    if ename == "0":
-        return "-1"
-    expense = esearch_e(expenses, ename)
-    while expense == None:
-        ename = input("Expense not found, enter an expense or 0 to go back\n")
-        if ename == "0":
-            return "-1"
-        expense = esearch_e(expenses, ename)
-    return expense
-
 def modify_expense(expenses):
+    #This function provides all the code and control flow for an expense to be modified
     expense = esearch(expenses)
     command = "0"
     if expense != "-1":
@@ -97,7 +95,21 @@ def modify_expense(expenses):
         expense.description = new_desc
         print("The description has been updated")
 
+def display_budget(expenses, funds):
+    #This function prints the budget for the user to analyse
+    remaining_funds = funds
+    print("\nStarting budget: " + str(funds))
+    for e in expenses:
+        new_funds = remaining_funds - e.cost
+        if e.description == e.name:
+            print(e.name + ":\n" + str(remaining_funds) + " - " + str(e.cost) + " = " + str(new_funds))
+        else:
+            print(e.name + ":\n"  + e.description + "\n"+ str(remaining_funds) + " - " + str(e.cost) + " = " + str(new_funds))
+        remaining_funds = new_funds
+    print("Total remaining funds: " + str(remaining_funds) + "\n")
+
 def make_budget(saved_expenses):
+    #This function creates a new budget, (funds and expenses) and calls manage budget to add to the budget
     f = input("Enter your funds\n")
     while re.match(r"\d",f) == None:
         f = input("Funds must be a number\n")
@@ -106,6 +118,7 @@ def make_budget(saved_expenses):
     manage_budget(funds,expenses,saved_expenses)
 
 def manage_budget(funds,expenses,saved_expenses):
+    #Control flow function for managing a budget
     command = input("Commands:\n1 - Add an expense, 2 - View the current budget, 3 - List saved expenses,\n4 - Modify budget expense, 5 - Modify saved expense, 0 - Go back\n")
     while command != "0":
         if command == "1":
@@ -122,17 +135,36 @@ def manage_budget(funds,expenses,saved_expenses):
             list_expenses(saved_expenses)
             modify_expense(saved_expenses)
             save_expenses(saved_expenses)
+        #elif command == "6":
+        #    if save_budget(expenses,funds) != 0:
+        #        print("Budget saved successfully")
         else:
             print("You have entered an incorrect command")
         command = input("Commands:\n1 - Add an expense, 2 - View the current budget, 3 - List saved expenses,\n4 - Modify budget expense, 5 - Modify saved expense, 0 - Go back\n")
 
 def save_expenses(expenses):
+    #Appends an expense to the file to be used across excecutions
     f = open('expenses.txt', 'w', encoding="utf-8")
     for e in expenses:
         f.write("n\n"+ e.name +"\nc\n" + str(e.cost) + "\nd\n" + e.description + "\n")
     f.close()
 
+def save_budget(expenses, funds):
+    #Writes a budget to a file
+    #Not yet used by the program
+    bname = input("Enter a name for the budget\n")
+    try:
+        f = open(str(bname + ".txt"), 'x', encoding="utf-8")
+    except OSError:
+        print("File already exists, try a different name")
+        return 0
+    f.write(str(funds) + "\n")
+    for e in expenses:
+        f.write("n\n"+ e.name +"\nc\n" + str(e.cost) + "\nd\n" + e.description + "\n")
+    f.close()
+
 def load_expenses():
+    #Loads the data from expenses file and saves to list of expenses
     expenses = []
     f = open('expenses.txt', 'r', encoding="utf-8")
     n,c,d = "",0,""
@@ -151,12 +183,13 @@ def load_expenses():
         line = f.readline()
     return expenses
 
-def clear_expenses(saved_expenses):
-    saved_expenses = []
+def clear_expenses(s):
+    #Clears file of expenses
     f = open('expenses.txt', 'w', encoding="utf-8")
     f.close()
 
 def manage_saved_expenses(saved_expenses):
+    #Control flow for managing saved expenses
     command = input("Commands: 1 - Add an expense, 2 - Modify expense, 3 - List expenses, 4 - Clear saved expenses, 0 - Go back\n")
     while command != "0":
         if command == "1":
@@ -169,7 +202,8 @@ def manage_saved_expenses(saved_expenses):
         elif command == "3":
             list_saved_expenses(saved_expenses)
         elif command == "4":
-            clear_expenses(saved_expenses)
+            saved_expenses = []
+            clear_expenses()
             print("Expenses cleared")
         else:
             print("Incorrect command entered")
@@ -191,6 +225,9 @@ def main():
         elif userin == "2":
             manage_saved_expenses(saved_expenses)
             saved_expenses = load_expenses()
+        #elif userin == "3":
+        #expenses,funds = [],0
+        #   load_budget(expenses,funds)
         else:
             print("You have entered an incorrect command")
         userin = input("Commands:\n1 - Make a new budget, 2 - Manage saved expenses, 0 - Quit.\n")
